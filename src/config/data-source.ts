d15c62d184ba2f120fd9c358c5bc51bf70627e2e
dy7791 <dy7791@naver.com>
2025-04-04 16:51:25 +0900

    init:: data-source

---
+++ src/config/data-source.ts
@@ -0,0 +1,16 @@
+import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || "3306"),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + "/../entities/**/*.entity.ts"],
+});
