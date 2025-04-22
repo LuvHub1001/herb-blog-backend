ba85c627a3ef045cde6f9d82cb0aa0a69316ea2d
dy7791 <dy7791@naver.com>
2025-04-22 15:11:02 +0900

    fix:: data-source

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -12,5 +12,9 @@ export const AppDataSource = new DataSource({
   database: process.env.DB_NAME,
   synchronize: true,
   logging: true,
-  entities: [__dirname + "/../entities/**/*.entity.ts"],
+  entities: [
+    process.env.NODE_ENV === "production"
+      ? __dirname + "/../dist/entities/**/*.entity.js"
+      : __dirname + "/../src/entities/**/*.entity.ts",
+  ],
 });
