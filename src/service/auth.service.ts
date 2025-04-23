639a7227e5fe68ad3f5d904e7d11814c7559a150
dy7791 <dy7791@naver.com>
2025-04-23 16:03:27 +0900

    remove

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -11,9 +11,6 @@ export const loginUser = async (id: string, password: string) => {
     throw new Error("존재하지 않는 사용자입니다.");
   }
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
     throw new Error("비밀번호가 일치하지 않습니다.");
